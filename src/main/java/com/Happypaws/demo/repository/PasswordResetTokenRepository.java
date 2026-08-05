b04c2addd70beb28d4e85a49e6e93ebf3e9338a6
Cristian Pinzon <cristian_0723@hotmail.com>
2026-08-05 17:35:56 -0500

    Add PasswordResetTokenRepository interface

---
+++ src/main/java/com/Happypaws/demo/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,9 @@
+package com.Happypaws.demo.repository;
+
+import com.Happypaws.demo.model.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+    Optional<PasswordResetToken> findByToken(String token);
+}
