7916d1769f981deda3d34cdbb8afeb1b28727343
Cristian Pinzon <cristian_0723@hotmail.com>
2026-08-05 18:49:22 -0500

    Add method to invalidate previous password reset tokens

--- src/main/java/com/Happypaws/demo/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/Happypaws/demo/repository/PasswordResetTokenRepository.java
@@ -1,9 +1,14 @@
-package com.Happypaws.demo.repository;
-
-import com.Happypaws.demo.model.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
     Optional<PasswordResetToken> findByToken(String token);
+
+    @Modifying
+    @Query("UPDATE PasswordResetToken t SET t.used = true WHERE t.user.id = :userId AND t.used = false")
+    void invalidarTokensAnteriores(@Param("userId") Long userId);
 }
