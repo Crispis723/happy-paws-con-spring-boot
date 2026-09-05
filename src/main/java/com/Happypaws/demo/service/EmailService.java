fba2e90a26548a2ff68fccf79069a14c241aa413
Cristian Pinzon <cristian_0723@hotmail.com>
2026-09-05 08:33:53 +0000

    fix: usar ObjectMapper de Jackson 3 con Spring Boot 4

--- src/main/java/com/Happypaws/demo/service/EmailService.java
+++ src/main/java/com/Happypaws/demo/service/EmailService.java
@@ -1,6 +1,6 @@
 package com.Happypaws.demo.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
